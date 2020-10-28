f76c648014b046609e7f5d235eaf7fef1cf1bd61
Mike Green <m.d.j.green@ntlworld.com>
2020-10-28 17:20:36 +0000

    Rudimentary Flask routing and Jinja template inheritance deployed

--- club_admin.py
+++ club_admin.py
@@ -1,6 +1,6 @@
 # msp3 club membership MDJG 28/10/2020
 import os
-from flask import Flask
+from flask import Flask, render_template
 if os.path.exists("env.py"):
     import env
 
@@ -8,8 +8,28 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def Hello():        # Test Flask works
-    return "Hello World...again!"   # Test message
+def membership():
+    return render_template("membership.html")
+
+
+@app.route("/members")
+def members():
+    return render_template("members.html")
+
+
+@app.route("/activities")
+def activities():
+    return render_template("activities.html")
+
+
+@app.route("/exhibition")
+def exhibition():
+    return render_template("exhibition.html")
+
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
 
 
 if __name__ == "__main__":
