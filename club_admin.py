a810ddeefa567baac4c59770792b55c2dd9f3feb
Mike Green <m.d.j.green@ntlworld.com>
2020-10-31 18:24:26 +0000

    Added login,logout and readonly via users collection

--- club_admin.py
+++ club_admin.py
@@ -1,8 +1,8 @@
 # msp3 club membership MDJG 28/10/2020
 import os
 from flask import (Flask, render_template,
-                redirect, request,
-                flash, url_for, session)
+                   redirect, request,
+                   flash, url_for, session)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -62,20 +62,60 @@ def register():
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            flash("Username {} already exists".format(request.form.get("username")))
+            flash("Username {} already exists".format(
+                request.form.get("username")))
             return redirect(url_for("register"))
 
         register = {
             "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
+            "password": generate_password_hash(request.form.get("password")),
+            "readonly": request.form.get("group1").lower()
         }
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
-        flash("Registration of {} Successful!".format(request.form.get("username")))
+        flash("Registration of {} Successful!".format(
+                        request.form.get("username")))
     return render_template("register.html",
-                            page_title="Administrator Registration")
+                           page_title="Administrator Registration")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for(
+                            "members", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html",
+                           page_title="Administrator Login")
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
