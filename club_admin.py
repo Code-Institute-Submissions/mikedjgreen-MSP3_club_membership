5c87cd690b0fb2b336f40f9ac9d0407788da8dd2
Mike Green <m.d.j.green@ntlworld.com>
2020-10-31 12:52:19 +0000

    Connected register form with database

--- club_admin.py
+++ club_admin.py
@@ -1,12 +1,24 @@
 # msp3 club membership MDJG 28/10/2020
 import os
-from flask import Flask, render_template, request, flash
+from flask import (Flask, render_template,
+                redirect, request,
+                flash, url_for, session)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
+
+
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+COLLECTION = "users"
+
+
+mongo = PyMongo(app)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -44,8 +56,26 @@ def gallery():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username {} already exists".format(request.form.get("username")))
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration of {} Successful!".format(request.form.get("username")))
     return render_template("register.html",
-                           page_title="Administrator Registration")
+                            page_title="Administrator Registration")
 
 
 if __name__ == "__main__":
